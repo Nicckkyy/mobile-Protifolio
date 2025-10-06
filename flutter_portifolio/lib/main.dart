636db28c32519c77a678d0c9b4fbff0fc61f7413
Nicole Camacho Rose <56914185808@>
2025-10-06 16:42:40 -0300

    06.10.2025 - Inicial page finish / 16:35

--- flutter_portifolio/lib/main.dart
+++ flutter_portifolio/lib/main.dart
@@ -1,21 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portifolio/home.dart';
+import 'package:flutter_portifolio/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyPortifolio());
+  runApp(const Inicial());
 }
 
-class MyPortifolio extends StatelessWidget {
-  const MyPortifolio({super.key});
+class Inicial extends StatelessWidget {
+  const Inicial({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyPortifolioHome(),
+    );
+  }
+}
+
+class MyPortifolioHome extends StatelessWidget {
+  const MyPortifolioHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Mobile - Portifolio")),
+        // appBar: AppBar(
+        //   title: Text(
+        //     "Mobile - Portifolio",
+        //     style: TextStyle(color: Color.fromRGBO(71, 55, 41, 1.0)),
+        //   ),
+        //   centerTitle: true,
+        //   backgroundColor: const Color.fromRGBO(107, 167, 153, 1.0),
+        //   toolbarHeight: 50,
+        // ),
         body: Center(
           child: Stack(
             children: [
-              
+              Image.asset(
+                "assets/images/chiriro-wallpaper.jpg",
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover, //cobre todo o espaço em branco da tela
+              ),
+
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Nick",
+                      style: TextStyle(
+                        color: Color.fromRGBO(56, 41, 15, 1),
+                        fontSize: 25,
+                        fontFamily: "Gabriela",
+                        letterSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Portifolio",
+                      style: TextStyle(
+                        color: Color.fromRGBO(56, 41, 15, 1),
+                        fontSize: 56,
+                        fontFamily: "Gabriela",
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white.withOpacity(0.25),
+                      ),
+                      child: Text(
+                        "more about me",
+                        style: TextStyle(
+                          color: Color.fromRGBO(2, 83, 77, 1),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
